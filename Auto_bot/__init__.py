4c2f9af9c59ec7e45aa07b975a68eb99d906ff38
thekinggf <95673827+thekinggf@users.noreply.github.com>
2021-12-08 12:46:07 +0530

    Create __init__.py

---
+++ Auto_bot/__init__.py
@@ -0,0 +1,35 @@
+from .Utils import (
+   get_filter_results,
+   get_file_details,
+   is_subscribed,
+   get_poster,
+   Media
+)
+from .Channel import (
+   RATING,
+   GENRES
+)
+
+HELP = """
+My Basic Commands
+/start :- check am Alive
+Bot Owner Only
+/broadcast Replay Any Message or Media
+/stats user Status
+/ban_user  click ban_user more info
+/unban_user click unban_user more info
+/banned_users Banned User Details
+/total How Many Files Added In Database
+/logger  Get Logs
+/delete Delete File From Database
+"""
+
+ABOUT = """
+➥ Name :  Auto Filter Bot V3
+    
+➥ Creator : [Dhyan K Narayanan](
+https://t.me/DhyanKN)
+➥ Language : Python3
+➥ Library : Pyrogram Asyncio 1.13.0
+➥ Channel : [Click Here](https://t.me/moviehousegf2)
+"""
